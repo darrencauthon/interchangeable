f6337b8e62adfb21f0caf7f2ab91bf7bfed48815
Darren Cauthon <darren@cauthon.com>
2014-10-14 09:55:37 -0500

    Have to include a description on the task.

--- lib/interchangeable/rake.rb
+++ lib/interchangeable/rake.rb
@@ -1,6 +1,7 @@
 require 'rake'
 
 namespace "interchangeable" do
+  desc "show the missing methods"
   task "missing_methods" do
     Kernel.puts Interchangeable::Tables.generate(Interchangeable.missing_methods)
   end
