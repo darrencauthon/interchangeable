ef70f4a453b5c4144ebbd8b3601e226510c8e884
Darren Cauthon <darren@cauthon.com>
2014-10-16 23:42:37 -0500

    This will not work. Starting to think that terminal table is not the right choice.

--- lib/interchangeable/tables.rb
+++ lib/interchangeable/tables.rb
@@ -1,7 +1,7 @@
 module Interchangeable
   module Tables
     def self.generate methods
-      Terminal::Table.new(headings: ['Class', 'Method', 'Description']) do |table|
+      Terminal::Table.new(headings: ['Class', 'Method', 'Description'], style: { width: 80 } ) do |table|
         methods.each do |method|
           table << [method.target, method.method_name, method.description]
         end
